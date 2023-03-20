6d51d7437bb24d7ffa165c5f27c771609ca03506
Vitaliy Pavlovich <vitalliypavlovich@gmail.com>
2023-03-20 21:55:24 +0300

    Update todo files

--- todo/settings.py
+++ todo/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "notes",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +70,8 @@ TEMPLATES = [
     },
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 WSGI_APPLICATION = 'todo.wsgi.application'
 
 
@@ -75,9 +79,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "todo",
+       "USER": "todo",
+       "PASSWORD": "todo",
+       "HOST": "localhost",
+       "PORT": 5432,
     }
 }
 
